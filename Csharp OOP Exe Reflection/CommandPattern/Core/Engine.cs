04cb04866a1a4c1cb75a090682fad450b0972c1b
kostadinM29 <68953020+kostadinM29@users.noreply.github.com>
2020-11-20 17:57:58 +0200

    Reflection Exe tasks

---
+++ Csharp OOP Exe Reflection/CommandPattern/Core/Engine.cs
@@ -0,0 +1,34 @@
+﻿using CommandPattern.Core.Contracts;
+using System;
+
+namespace CommandPattern
+{
+    public class Engine : IEngine
+    {
+        private readonly ICommandInterpreter command;
+
+        public Engine(ICommandInterpreter command)
+        {
+            this.command = command;
+        }
+
+        public void Run()
+        {
+            while (true)
+            {
+                string args = Console.ReadLine();
+
+                try
+                {
+                    string result = this.command.Read(args);
+
+                    Console.WriteLine(result);
+                }
+                catch (ArgumentException argumentException)
+                {
+                    Console.WriteLine(argumentException.Message);
+                }
+            }
+        }
+    }
+}
